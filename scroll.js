1e88b85bc9ef82ac280958098f40e3a22f4c8c40
Luke1836 <codingluke2023@gmail.com>
2024-01-30 20:25:44 +0530

    Cursor animation js file modified

--- scroll.js
+++ scroll.js
@@ -100,3 +100,18 @@ const observer = new IntersectionObserver((entries) => {
 })
 const hiddenSections = document.querySelectorAll('.hidden');
 hiddenSections.forEach((ele) => observer.observe(ele));
+
+/* Cursor animation and movement */
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;");
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove('expand');
+    }, 500);
+})
